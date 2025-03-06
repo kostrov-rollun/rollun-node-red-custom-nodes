1b2ac39359acdbf60d803de692800d702478dd5f
kostrov-rollun <kostrov.rollun@gmail.com>
2025-03-06 11:30:21 +0000

    feat(28250): update all sp-api-models, add script to update models

--- README.md
+++ README.md
@@ -8,3 +8,10 @@ npm run start:dev
 ```
 in project root, this will open up node-red on http://localhost:1880
 with all custom nodes installed
+
+### How to update SP-API models
+
+```sh
+chmod +x scripts/update-sp-api-models.sh
+./scripts/update-sp-api-models.sh
+```
---
+++ scripts/update-sp-api-models.sh
@@ -0,0 +1,5 @@
+# Delete existing directory using relative path from project root
+rm -rf node-red-contrib-rollun-aws-sp-api/selling-partner-api-models
+
+# Clone into correct location using relative path
+git clone https://github.com/amzn/selling-partner-api-models.git node-red-contrib-rollun-aws-sp-api/selling-partner-api-models
