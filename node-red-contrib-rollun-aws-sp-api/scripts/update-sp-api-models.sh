c235d0280e287b1fc05d43ee167c3ae1bbd5ca20
kostrov-rollun <kostrov.rollun@gmail.com>
2025-03-06 12:40:30 +0000

    feat(28250): change update models script logic

--- node-red-contrib-rollun-aws-sp-api/scripts/update-sp-api-models.sh
+++ node-red-contrib-rollun-aws-sp-api/scripts/update-sp-api-models.sh
@@ -1,17 +1,7 @@
-# Delete existing directory using relative path from project root
 rm -rf selling-partner-api-models
 
-# Clone into correct location using relative path
-git clone https://github.com/amzn/selling-partner-api-models.git
+git clone https://github.com/amzn/selling-partner-api-models.git tmp
 
-# Remove unnecessary directories
-rm -rf selling-partner-api-models/clients
-rm -rf selling-partner-api-models/.github
-rm -rf selling-partner-api-models/schemas
-
-# Remove unnecessary files
-rm -rf selling-partner-api-models/CODE_OF_CONDUCT.md
-rm -rf selling-partner-api-models/CONTRIBUTING.md
-rm -rf selling-partner-api-models/LICENSE
-rm -rf selling-partner-api-models/NOTICE
-rm -rf selling-partner-api-models/README.md
+mkdir selling-partner-api-models
+mv tmp/models selling-partner-api-models/models
+rm -rf tmp
